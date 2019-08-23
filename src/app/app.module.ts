845f8f3ee4f5abc8047667e42a33ca486feef02f
Bryce Thorson <brycethorson.design@gmail.com>
2019-08-23 09:31:26 -0400

    added homepage, header, footer, and content-page components. added sticky module to page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentPageComponent } from './content-page/content-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentPageComponent
   ],
   imports: [
     BrowserModule,
